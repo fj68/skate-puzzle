9967876ea05bdb7e11ebddd9aece86803c07e4da
Shintaro K <fj68forg@gmail.com>
2025-03-19 05:06:54 +0000

    change to use Set for H/V-walls

--- src/hyper/mapdata.ts
+++ src/hyper/mapdata.ts
@@ -3,25 +3,40 @@ import type { IStringer } from "./IStringer";
 import { Size } from "./size";
 import { Rectangle } from "./rectangle";
 import * as Arrays from "./arrays";
+import { Position } from "./position";
 
 export class Mapdata implements IEqualable<Mapdata>, IStringer {
-  horizontalWalls: number[][];
-  verticalWalls: number[][];
+  horizontalWalls: Set<number>[];
+  verticalWalls: Set<number>[];
   constructor(public size: Size) {
-    this.horizontalWalls = new Array(size.width).fill(0).map(() => []);
-    this.verticalWalls = new Array(size.height).fill(0).map(() => []);
+    this.horizontalWalls = new Array(size.width)
+      .fill(0)
+      .map(() => new Set<number>());
+    this.verticalWalls = new Array(size.height)
+      .fill(0)
+      .map(() => new Set<number>());
+  }
+  putHorizontalWall(position: Position): void {
+    this.horizontalWalls[position.x].add(position.y);
+  }
+  putVerticalWall(position: Position): void {
+    this.verticalWalls[position.y].add(position.x);
   }
   get center(): Rectangle {
     return Rectangle.of(this.size.center.floor(), new Size(2, 2));
   }
   equals(other: Mapdata): boolean {
+    const setEquals =
+      <T>(eq: (x: T, y: T) => boolean) =>
+      (xs: Set<T>, ys: Set<T>): boolean =>
+        xs.size === ys.size && Array.from(xs.values()).every((x) => ys.has(x));
     return (
       this.size.equals(other.size) &&
-      Arrays.equals(Arrays.equals((x, y) => x === y))(
+      Arrays.equals(setEquals((x, y) => x === y))(
         this.horizontalWalls,
         other.horizontalWalls
       ) &&
-      Arrays.equals(Arrays.equals((x, y) => x === y))(
+      Arrays.equals(setEquals((x, y) => x === y))(
         this.verticalWalls,
         other.verticalWalls
       )
